56972e77592bd3002c52ab3c1fba94cf76f99a56
suptoasty <jason.lonsinger@gmail.com>
2019-03-07 17:06:24 -0600

    Added in Lanes Teleport Controller

---
+++ Game/Assets/Scripts/Teleporting/Teleporter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Teleporter : MonoBehaviour
+{
+    static public float heightCovered = 0.6F;
+    Camera cam;
+    CharacterController controller;
+    GameObject sphere;
+    Transform sphereTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphere = GameObject.FindWithTag("Sphere");
+        if (sphere != null)
+        {
+            sphereTransform = sphere.GetComponent<Transform>();
+        }
+        cam = GetComponentInChildren<Camera>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    //returns the coordinates of the reticule teleport location
+    //NOTE: will not work to teleport above a ledge that the player cannot see
+    public Vector3 teleport(float maxDistance, CharacterController originalCollider, Vector3 direction, Vector3 viewPos)
+    {
+        //set the top to the top of the caller's capsule collider
+        float topY = originalCollider.bounds.center.y + originalCollider.height / 2;
+
+        //set the bottom to the bottom of the caller's capsule collider
+        float bottomY = originalCollider.bounds.center.y - originalCollider.height / 2;
+
+        //set the height of the bottom ray in relation to the caller's capsule collider's height
+        float fractionHeight = originalCollider.height * heightCovered;
+
+        //set the origins of the top and bottom rays. set their direction to be the same
+        Vector3 bottomRayLoc = new Vector3(originalCollider.bounds.center.x, topY - fractionHeight, originalCollider.bounds.center.z);
+        Vector3 topRayLoc = new Vector3(originalCollider.bounds.center.x, viewPos.y, originalCollider.bounds.center.z);
+        Ray bottomRay = new Ray(bottomRayLoc, direction);
+        Ray topRay = new Ray(topRayLoc, direction);
+
+        //Fire a ray from the top and another from the bottom. The shortest distance is prioritized.
+        RaycastHit bottomHit;
+        RaycastHit topHit;
+        Vector3 teleportLoc = new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity); //initialized to an impossible value
+        float heightDifference = viewPos.y - (topY - fractionHeight);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Debugging");
+        }
+
+        if (Physics.Raycast(bottomRay, out bottomHit, maxDistance))
+        {
+            Vector3 point = new Vector3(bottomHit.point.x, bottomHit.point.y, bottomHit.point.z);
+            point.y += heightDifference;
+            teleportLoc = point;
+            Debug.Log("Bottom");
+        }
+        if(Physics.Raycast(topRay, out topHit, maxDistance))
+        {
+            Vector3 point = new Vector3(topHit.point.x, topHit.point.y, topHit.point.z);
+            float topDist = Math.Abs(Vector3.Distance(viewPos, point));
+
+            float bottomDist = Math.Abs(Vector3.Distance(viewPos, teleportLoc));
+
+            if (teleportLoc.x != float.PositiveInfinity)
+            {
+                if (Math.Abs(Vector3.Distance(viewPos, point)) < Math.Abs(Vector3.Distance(viewPos, teleportLoc)))
+                {
+                    teleportLoc = point;
+                    Debug.Log("Top");
+                }
+            }
+        }
+        if (teleportLoc.x == float.PositiveInfinity)
+        {
+            Vector3 origin = new Vector3(viewPos.x, viewPos.y, viewPos.z);
+            Vector3 endPoint = new Vector3(viewPos.x, viewPos.y, viewPos.z);
+            endPoint.x += direction.x * maxDistance;
+            endPoint.y += direction.y * maxDistance;
+            endPoint.z += direction.z * maxDistance;
+            teleportLoc = endPoint;
+        }
+        sphereTransform.position = teleportLoc;
+        return teleportLoc;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray cameraRay = cam.ScreenPointToRay(new Vector3(cam.pixelWidth /2, cam.pixelHeight / 2, 0));
+        Vector3 cameraDirection = cameraRay.direction;
+        teleport(20F, controller, cameraDirection, cam.transform.position);
+    }
+}
