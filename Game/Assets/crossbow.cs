6b178a5be27ec6d839ba405dc65ee3d8753446fe
suptoasty <jason.lonsinger@gmail.com>
2019-03-12 12:17:53 -0500

    added som logic for gun script

---
+++ Game/Assets/crossbow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crossbow : MonoBehaviour
+{
+    public float damage = 1.0f; //damage to be taken in by enemy script passed to function like? takedamage(damage);???
+    public float distance = 200.0f; //max distance for a hit
+
+    public Camera fpsCamera; //must be set to player camera.....might look at changing to ray from hand
+
+    public ParticleSystem boltImpactSmoke;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+            shoot();
+        }
+    }
+
+    public void shoot() {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, distance)) {
+            Debug.log(hit.transform.name); //name of object hit
+        }
+
+    }
+}
