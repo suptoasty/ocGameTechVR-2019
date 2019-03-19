495498b2804fe8806d01945c23c757b69c241651
suptoasty <jason.lonsinger@gmail.com>
2019-03-18 22:10:52 -0500

    added script for enemy,tag, and layer. made arrow script. enemies can now take damage and are removed when health is less than or equal to zero

---
+++ Game/Assets/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int max_health = 3;
+    private int health = 3;
+    public ParticleSystem death_particles = null;
+    public float death_delay_time = 1.0f; //for testing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = max_health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead())
+        {
+            //might need to access parent and instantiate death particles under that
+            if (death_particles)
+            {
+                death_particles.Play();
+            }
+            Destroy(this.gameObject, death_delay_time);
+        }
+    }
+
+    public void takeDamage(float damage)
+    {
+        int damage_taken = (int)damage;
+        health -= damage_taken;
+        Debug.Log(name + " health-> " + getHealth());
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+    public bool isDead()
+    {
+        if (health <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Game/Assets/arrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float arrow_damage = 1.0f;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        float damage = arrow_damage;
+        // Debug.Log(name + " Hit-> " + collisionInfo.collider.name);
+        if (collisionInfo.collider.tag == "Enemy")
+        {
+            //get collider as enemy and have it take damage
+            GameObject enemyObj = collisionInfo.gameObject as GameObject;
+            Enemy enemy = enemyObj.GetComponent<Enemy>() as Enemy;
+            enemy.takeDamage(damage);
+            //get parent to kill object
+            Destroy(this.gameObject);
+        }
+    }
+}
