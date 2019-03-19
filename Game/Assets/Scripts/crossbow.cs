fa0f0f65de432455c9b841e2b01a8586e9ce814a
suptoasty <jason.lonsinger@gmail.com>
2019-03-18 20:15:40 -0500

    Fixed arrows firing with same direction

--- Game/Assets/Scripts/crossbow.cs
+++ Game/Assets/Scripts/crossbow.cs
@@ -9,7 +9,7 @@ public class crossbow : MonoBehaviour
     public float damage = 1.0f; //damage to be taken in by enemy script passed to function like? takedamage(damage);???
     // public float distance = 200.0f; //max distance for a hit
     public float speedAffect = 10.0f;
-
+    public int max_arrow_inscene = 2;
     // public Camera fpsCamera; //must be set to player camera.....might look at changing to ray from hand
 
     // public ParticleSystem boltImpactSmoke;
@@ -19,15 +19,12 @@ public class crossbow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // if (lineRenderer)
-        // {
-        //     lineRenderer.enabled = true;
-        // }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //below is for testing purposes...remove for final game
         if (Input.GetButtonDown("Fire1"))
         {
             shoot();
@@ -44,18 +41,14 @@ public class crossbow : MonoBehaviour
 
     public void shoot()
     {
-        // RaycastHit hit;
-        // if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, distance))
-        // {
-        //     Debug.Log(hit.transform.name); //name of object hit
-        // }
         if (projectile)
         {
+            //a child is used for positions and rotations rather than the crossbow
             projectile.transform.localScale = transform.localScale; //scale projectiles to proportional size of the crossbow...still a little off 
-            projectile.transform.rotation = new Quaternion();
+            projectile.transform.rotation = transform.GetChild(0).transform.rotation; //sets rotation of projectile
             GameObject bullet = Instantiate(projectile, transform.GetChild(0).gameObject.transform.position, transform.GetChild(0).gameObject.transform.rotation) as GameObject; //instantiate in the scene
-            Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponentInChildren<Collider>());
-            bullet.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, 0.0f, 1.0f) * speedAffect, ForceMode.Impulse); //apply impulse to move
+            Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponentInChildren<Collider>()); //prevents arrow from colliding with crossbow and its siblings under the crossbow
+            bullet.GetComponent<Rigidbody>().AddForce(transform.GetChild(0).forward * speedAffect, ForceMode.Impulse); //apply impulse to move
             Destroy(bullet, projectileLifeTime); //remove bullets after 3 seconds
         }
         else
