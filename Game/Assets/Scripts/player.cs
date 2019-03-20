7fbc68155984100676bc9a7d76ac6198ce88debe
suptoasty <jason.lonsinger@gmail.com>
2019-03-19 19:32:39 -0500

    moved all scirpts for game to scripts folder, added player script for health and damage

---
+++ Game/Assets/Scripts/player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public int max_health = 3;
+    private int health = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = max_health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead())
+        {
+            Destroy(this, 0.0f);
+        }
+    }
+
+
+
+    public void takeDamage(float damage)
+    {
+        int damage_taken = (int)damage;
+        health -= damage_taken;
+    }
+
+    bool isDead()
+    {
+        if (health <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
