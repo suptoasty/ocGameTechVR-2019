edd38f40f43aea99e1215b91c1b95fe374435d98
suptoasty <jason.lonsinger@gmail.com>
2019-03-23 11:56:33 -0500

    Work on Enemy Ai detecting player

--- Game/Assets/Scripts/EnemyPatrolState.cs
+++ Game/Assets/Scripts/EnemyPatrolState.cs
@@ -8,6 +8,8 @@ public class EnemyPatrolState : State<Enemy>
 {
     //uses singleton pattern to ensure only one instance of script is returned
     private static EnemyPatrolState _singleton;
+    private Enemy owner = null;
+    private MeshCollider cone = null;
 
     private EnemyPatrolState()
     {
@@ -29,15 +31,21 @@ public class EnemyPatrolState : State<Enemy>
             return _singleton;
         }
     }
-
     public override void EnterState(Enemy _owner)
     {
+        owner = _owner;
+        GameObject coneObj = GameObject.Find("sweepCast");
+        cone = coneObj.GetComponent<MeshCollider>() as MeshCollider;
+
 
     }
     public override void ExitState(Enemy _owner)
     {
-
+        cone = null;
+        owner = null;
     }
+
+    //events to change state are....has cone collided with player, if so then do a ray cast at that point, is the first object hit by ray player, if yes hunt, else keep patroling
     public override void UpdateState(Enemy _owner)
     {
 
