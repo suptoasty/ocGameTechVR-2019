3b35a693a17f47f5eceb09b8c627d1160787b2b6
SimpsonDeveloper <35541945+SimpsonDeveloper@users.noreply.github.com>
2019-03-24 16:37:51 -0500

    ACTUALLY Fixed Arrow Rotation. Crossbow now uses a new prefab called Arrow2 instead of Arrow as its projectile. Arrow scale is open for tweaking.

--- Game/Assets/Scripts/crossbow.cs
+++ Game/Assets/Scripts/crossbow.cs
@@ -45,8 +45,9 @@ public class crossbow : MonoBehaviour
         if (projectile)
         {
             //a child is used for positions and rotations rather than the crossbow
-            projectile.transform.localScale = transform.localScale / 2; //scale projectiles to proportional size of the crossbow...still a little off 
-            Quaternion projRot = Quaternion.Euler(Random.Range(45, 145), 90, Random.Range(80, 100)); //fix rotation of arrow
+            projectile.transform.localScale = transform.localScale * 4; //scale projectiles to proportional size of the crossbow...still a little off 
+            //Quaternion projRot = Quaternion.Euler(Random.Range(45, 145), 90, Random.Range(80, 100)); //fix rotation of arrow
+            Quaternion projRot = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z); //fix rotation of arrow based on crossbow rotation
             GameObject bullet = Instantiate(projectile, transform.GetChild(0).gameObject.transform.position, projRot) as GameObject; //instantiate in the scene
             Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponentInChildren<Collider>()); //prevents arrow from colliding with crossbow and its siblings under the crossbow
             bullet.GetComponent<Rigidbody>().AddForce(transform.GetChild(0).forward * speedAffect, ForceMode.Impulse); //apply impulse to move
